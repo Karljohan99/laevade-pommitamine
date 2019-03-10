cd169a0c75342526f64c52458deabbe447c033b9
Karljohan99 <karljohan30@gmail.com>
2019-03-10 15:43:16 +0200

    Lisan mängulaua

---
+++ src/main/java/ee/ut/cs/courses/oop/lp/Mängulaud.java
@@ -0,0 +1,11 @@
+package ee.ut.cs.courses.oop.lp;
+
+public class Mängulaud {
+    StringBuilder sb = new StringBuilder("   A B C D E F G H I J   \n" + "10 * * * * * * * * * * 10\n" + "9  * * * * * * * * * * 9 \n" +
+            "8  * * * * * * * * * * 8 \n" + "7  * * * * * * * * * * 7 \n" + "6  * * * * * * * * * * 6 \n" +
+            "5  * * * * * * * * * * 5 \n" + "4  * * * * * * * * * * 4 \n" + "3  * * * * * * * * * * 3 \n" +
+            "2  * * * * * * * * * * 2 \n" + "1  * * * * * * * * * * 1 \n" + "   A B C D E F G H I J" );
+    public String toString() {
+        return sb.toString();
+   }
+}
