b0e9eb3bb16ccdf694b5a51da218dbd65af6c20f
Eero Pihelgas <eero.pihelgas@gmail.com>
2019-03-16 23:10:09 +0200

    Lisan laevade genereerimise

--- src/main/java/ee/ut/cs/courses/oop/lp/Mängulaev.java
+++ src/main/java/ee/ut/cs/courses/oop/lp/Mängulaev.java
@@ -1,16 +1,62 @@
 package ee.ut.cs.courses.oop.lp;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Mängulaev {
 
     private final Positsioon[] positsioonid;
 
-    public Mängulaev(Positsioon[] positsioonid) {
-        this.positsioonid = positsioonid;
+    public Mängulaev(int x, int y, int suurus) {
+        this.positsioonid = new Positsioon[suurus];
+        int dx = ThreadLocalRandom.current().nextInt(2);
+        int dy = dx == 0 ? 1 : 0;
+        for (int i = 0; i < suurus; i++) {
+            this.positsioonid[i] = new Positsioon(x, y);
+            x += dx;
+            y += dy;
+        }
+    }
 
+    public boolean asubMängulaual() {
+        for (Positsioon positsioon : this.getPositsioonid()) {
+            if (positsioon.getX() >= Mängulaud.SUURUS || positsioon.getY() >= Mängulaud.SUURUS) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public Positsioon[] getPositsioonid() {
         return this.positsioonid;
+    }
+
+    public boolean kattub(int x, int y) {
+        for (Positsioon positsioon : this.getPositsioonid()) {
+            if (positsioon.kattub(x, y)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean kattub(Mängulaev laev) {
+        if (laev != null) {
+            for (Positsioon positsioon : this.getPositsioonid()) {
+                if (positsioon.kattub(laev.getPositsioonid())) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean kattub(Mängulaev[] laevad) {
+        for (Mängulaev laev : laevad) {
+            if (this.kattub(laev)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public boolean onHävitatud() {
@@ -20,6 +66,10 @@ public class Mängulaev {
             }
         }
         return true;
+    }
+
+    public int suurus() {
+        return this.getPositsioonid().length;
     }
 
     public static class Positsioon {
@@ -43,6 +93,23 @@ public class Mängulaev {
 
         public void hävita() {
             this.hävitatud = true;
+        }
+
+        public boolean kattub(int x, int y) {
+            return this.getX() == x && this.getY() == y;
+        }
+
+        public boolean kattub(Positsioon positsioon) {
+            return positsioon != null && this.kattub(positsioon.getX(), positsioon.getY());
+        }
+
+        public boolean kattub(Positsioon[] positsioonid) {
+            for (Positsioon positsioon : positsioonid) {
+                if (this.kattub(positsioon)) {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public boolean onHävitatud() {
--- src/main/java/ee/ut/cs/courses/oop/lp/Mängulaud.java
+++ src/main/java/ee/ut/cs/courses/oop/lp/Mängulaud.java
@@ -1,15 +1,44 @@
 package ee.ut.cs.courses.oop.lp;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Mängulaud {
+
+    public static final int SUURUS = 10;
+
+    private final Mängulaev[] laevad = new Mängulaev[Mängulaud.SUURUS];
+
+    public Mängulaud() {
+        int[] suurused = {4, 3, 3, 2, 2, 2, 1, 1, 1, 1};
+        for (int i = 0; i < Mängulaud.SUURUS; i++) {
+            int suurus = suurused[i];
+            while (this.laevad[i] == null) {
+                int x = ThreadLocalRandom.current().nextInt(Mängulaud.SUURUS);
+                int y = ThreadLocalRandom.current().nextInt(Mängulaud.SUURUS);
+                Mängulaev laev = new Mängulaev(x, y, suurus);
+                if (!laev.asubMängulaual()) {
+                    continue;
+                }
+                if (laev.kattub(this.laevad)) {
+                    continue;
+                }
+                this.laevad[i] = laev;
+            }
+        }
+    }
+
+    public Mängulaev[] getLaevad() {
+        return this.laevad;
+    }
 
     @Override
     public String toString() {
         String tähed = "  A B C D E F G H I J";
         StringBuilder sb = new StringBuilder();
         sb.append(tähed).append(System.lineSeparator());
-        for (int y = 0; y < 10; y++) {
+        for (int y = 0; y < Mängulaud.SUURUS; y++) {
             sb.append(y);
-            for (int x = 0; x < 10; x++) {
+            for (int x = 0; x < Mängulaud.SUURUS; x++) {
                 sb.append(" *");
             }
             sb.append(" ").append(y).append(System.lineSeparator());
